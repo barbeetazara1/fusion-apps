e049311701f3130f9072fabe6455545e569dda6e
barbeetazara <baronbetazara@gmail.com>
2024-09-23 23:03:27 +0700

    [FIX] Login Page

--- fusion_custom/__manifest__.py
+++ fusion_custom/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "fusion_custom",
+    'name': "Fusion Custom",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Fusion Custom General",
 
     'description': """
-Long description of module's purpose
+        Fusion Custom For General
     """,
 
     'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'website': "https://internal-fusion-erp.site",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -18,17 +18,21 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['web'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/login_page.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'fusion_custom/static/src/scss/login_page.scss',
+        ],
+    }
 }
 
